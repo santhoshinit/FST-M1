79d88feb6a7888d50a2bc689100e570878cec59e
santhoshinit <santhoshinit@gmail.com>
2021-10-05 13:23:20 -0500

    Add files via upload

---
+++ Cucumber/Activities/Activity6_ActivitiesRunner_Prettyreport.java
@@ -0,0 +1,20 @@
+package cucumberTest;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "Features",
+    glue = {"stepDefinitions"},
+    tags = "@SimpleAlert",
+    plugin = {"pretty"},
+    monochrome = true
+)
+
+
+public class Activity6_ActivitiesRunner_Prettyreport {
+
+}
